73d0491b6fb2721c7a3c9c82008f9f5f5267aa39
aditi970 <76680351+aditi970@users.noreply.github.com>
2022-11-07 12:39:39 +0530

    Create adjacentelements.py

---
+++ adjacentelements.py
@@ -0,0 +1,12 @@
+list=[] 
+count=0 n = int(input("Enter the no. of Element: ")) 
+for i in range(n):        
+x = int(input("Element in list: "))       
+list.append(x)
+print(list) 
+i=0
+while(i<n-1):       
+if list[i]==list[i+1]: 
+         count+=1       
+         i+=1 
+         print(count) 
