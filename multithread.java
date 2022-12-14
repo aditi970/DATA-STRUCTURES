4e2c076dc3d9b92591cfd21f7eadb47a10b0d4ad
aditi970 <76680351+aditi970@users.noreply.github.com>
2022-12-14 21:47:31 +0530

    Create multithread.java

---
+++ multithread.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+      class Square extends Thread
+    {
+         int x;
+      Square(int n)
+      {
+          x = n;
+        }
+          public void run()
+         {
+                  int sqr = x * x;
+                System.out.println("Square of " + x + " = " + sqr );
+       }
+}
+class Cube extends Thread
+{
+int x;
+Cube(int n);
+{
+x = n;
+}
+public void run()
+{
+int cub = x * x * x;
+System.out.println("Cube of " + x + " = " + cub );}}
+class Number extends Thread
+{public void run()
+{201B358
+Random random = new Random();
+for(int i =0; i<5; i++)
+{int randomInteger = random.nextInt(100);
+System.out.println("Random Integer generated : " + randomInteger);
+Square s = new Square(randomInteger);
+s.start()
+Cube c = new Cube(randomInteger);
+c.start();
+try {
+Thread.sleep(1000);
+} catch (InterruptedException ex) {
+System.out.println(ex);
+}}}}
+public class Thr {
+public static void main(String args[])
+{Number n = new Number();
+n.start();
+}}
